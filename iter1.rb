35fc903a8e80880d37696fc1b317d6d2648797da
marlen713 <ma000161@gmail.com>
2022-04-05 18:55:36 -0400

    finishead:iter1

--- iter1.rb
+++ iter1.rb
@@ -1,1 +1,25 @@
-ruby iter1.rb
+ventas = {
+Enero: 15000,
+Febrero: 22000,
+Marzo: 12000,
+Abril: 17000,
+Mayo: 81000,
+Junio: 13000,
+Julio: 21000,
+Agosto: 41200,
+Septiembre: 25000,
+Octubre: 21500,
+Noviembre: 91000,
+Diciembre: 21000
+}
+# Crear el programa iter1.rb que itere el hash ventas y muestre en pantalla todas los ventas
+# superiores a 45000 (sólo el valor de la venta).
+#Para  seleccionar los mejores metodos son Selec y Reject
+#Select devuelve la seleccion y Reject devuelve lo rechazado
+
+selected_sales = ventas.select do |key, value|
+    value > 45000
+end
+
+print selected_sales.values
+puts
