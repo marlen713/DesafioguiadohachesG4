d94a3e58b37bc61d0662fc16b680c6b7b4ca7c22
marlen713 <ma000161@gmail.com>
2022-04-05 20:47:43 -0400

    Finishead: busqueda.rb

---
+++ busqueda.rb
@@ -0,0 +1,53 @@
+ventas = {
+Enero: 15000,
+Febrero: 22000,
+Marzo: 12000,
+Abril: 17000,
+Mayo: 81000,
+Junio: 13000,
+Julio: 21000,
+Agosto: 41200,
+Septiembre: 25000,
+Octubre: 21500,
+Noviembre: 91000,
+Diciembre: 21000
+}
+
+# Crear un programa llamado busqueda.rb que pueda buscar a que mes pertenece una o más
+# cifras específicas. En caso de no encontrarlo mostrar el mensaje "no encontrado".
+
+# ruby busqueda.rb 15000 31000 27000 ARGV
+
+search_data = ARGV.map do |element|
+    element.to_i
+end
+#=> {15000=>:Enero 31000 27000}
+#=> Enero, no encontrado, no encontrado
+# sales_inverted = ventas.invert
+# #=> {15000=>:Enero, 22000=>:Febrero, 12000=>:Marzo, 17000=>:Abril, 81000=>:Mayo, 13000=>}
+# search_data.each do |sale|
+#     #evaluar si la llave sale existe o es nil
+#     #si existe debe devolver el valor de la llave
+#     if sales_inverted[sale]
+#         puts sales_inverted[sale]
+#     else
+#     #osea que si es nulo debe imprimir "no encontrado"
+#     puts "no encontrado"    
+#     end
+#     puts (sales_inverted[sale])? (sales_inverted[sale]) : ("no encontrado")
+# end
+
+# puts sales_inverted[15000]
+founded =false
+search_data.each do |sale|
+    ventas.each do |key, value|
+        if sale == value
+            puts key
+            founded = true
+        end
+    end
+    if founded == false
+        puts "no encontrado"
+    end
+    founded = false
+end
